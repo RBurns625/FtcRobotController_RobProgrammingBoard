f2f657af303a7eeb7644e170efe6e19f8d854f43
RBurns625 <rburns625@icloud.com>
2024-11-01 11:05:09 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TouchSensorOpMode.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBoard;
+
+@SuppressWarnings("unused")
+@TeleOp
+
+public class TouchSensorOpMode extends OpMode {
+    ProgrammingBoard board = new ProgrammingBoard();
+
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Touch Sensor", board.getTouchSensorState());
+    }
+}
