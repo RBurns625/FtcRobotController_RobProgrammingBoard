0836e6276933ca86ca23fb8468c9a05b161a796b
RBurns625 <rburns625@icloud.com>
2024-11-01 13:11:55 -0700

    added motor method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBoard.java
@@ -1,22 +1,37 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @SuppressWarnings("unused")
 public class ProgrammingBoard {
     private DigitalChannel touchSensor;
+    private DcMotor motor;
+    private double ticksPerRotation;
 
     public void init(HardwareMap hwMap) {
+        // Touch Sensor Settings
         touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
+        // Motor Settings
+        motor = hwMap.get(DcMotor.class, "motor");
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ticksPerRotation = motor.getMotorType().getTicksPerRev();
     }
-
-    public boolean isTouchSensorPressed() {
+        public boolean isTouchSensorPressed() {
         return !touchSensor.getState();
     }
-
     public boolean isTouchSensorReleased() {
         return touchSensor.getState();
+    }
+    public void setMotorSpeed(double speed){
+        motor.setPower(speed);
+    }
+    public double getMotorRotations() {
+        return motor.getCurrentPosition() / ticksPerRotation;
+    }
+    public void setMotorZeroBehavior(DcMotor.ZeroPowerBehavior zeroBehavior) {
+        motor.setZeroPowerBehavior(zeroBehavior);
     }
 }
