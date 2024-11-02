b14fd1063feece5ec090da334169bc0b8e775f4e
RBurns625 <rburns625@icloud.com>
2024-11-01 17:45:44 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/rgbLightOpMode.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBoard;
+
+@SuppressWarnings("unused")
+@TeleOp
+
+public class rgbLightOpMode extends OpMode {
+    ProgrammingBoard board = new ProgrammingBoard();
+
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+        telemetry.addLine("Press the following buttons");
+        telemetry.addLine("A: Red");
+        telemetry.addLine("B: Yellow");
+        telemetry.addLine("X: Green");
+        telemetry.addLine("Y: Blue");
+        telemetry.addLine("Use 'Right Trigger' to go through spectrum");    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.a) {
+            board.setServoPosition(0.277);
+            telemetry.addLine("Color: Red");
+        }
+        else if(gamepad1.b) {
+            board.setServoPosition(0.388);
+            telemetry.addLine("Color: Yellow");
+        }
+        else if(gamepad1.x) {
+            board.setServoPosition(0.500);
+            telemetry.addLine("Color: Green");
+        }
+        else if(gamepad1.y) {
+            board.setServoPosition(0.611);
+            telemetry.addLine("Color: Blue");
+        }
+        else board.setServoPosition(0.0);
+
+        board.setServoPosition(gamepad1.right_trigger);
+        telemetry.addData("Servo Position", board.getServoPosition());
+        telemetry.update();
+
+    }
+}
