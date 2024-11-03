7f9570c5b39f727b547acd88a2293db39118cb8d
RBurns625 <rburns625@icloud.com>
2024-11-03 15:06:47 -0700

    edited for new class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RgbLightOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RgbLightOpMode.java
@@ -3,46 +3,44 @@ package org.firstinspires.ftc.teamcode.OpModes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBoard;
+import org.firstinspires.ftc.teamcode.subassembly.GoBildaRGBLight;
 
 @SuppressWarnings("unused")
 @TeleOp
 
 public class RgbLightOpMode extends OpMode {
-    ProgrammingBoard board = new ProgrammingBoard();
+    GoBildaRGBLight light = new GoBildaRGBLight();
 
     @Override
     public void init() {
-        board.init(hardwareMap);
-        board.setServoPosition(0.0);
+        light.init(hardwareMap);
     }
 
     @Override
     public void loop() {
+        if (gamepad1.a)  light.setColorRed();
+        if (gamepad1.b)  light.setColorYellow();
+        if (gamepad1.x)  light.setColorGreen();
+        if (gamepad1.y)  light.setColorBlue();
+        if (gamepad1.dpad_up)    light.setColorOrange();
+        if (gamepad1.dpad_left)  light.setColorSage();
+        if (gamepad1.dpad_right) light.setColorAzure();
+        if (gamepad1.dpad_down)  light.setColorIndigo();
+        if (gamepad1.left_stick_button)  light.setColorBlack();
 
-        if(gamepad1.a) board.setServoPosition(0.277);
-        if (gamepad1.b) board.setServoPosition(0.388);
-        if (gamepad1.x) board.setServoPosition(0.500);
-        if (gamepad1.y) board.setServoPosition(0.611);
-        if (gamepad1.left_stick_button) board.setServoPosition(0.0);
-        if (gamepad1.dpad_up) board.setServoPosition(0.333);
-        if (gamepad1.dpad_left) board.setServoPosition(0.444);
-        if (gamepad1.dpad_right) board.setServoPosition(0.555);
-        if (gamepad1.dpad_down) board.setServoPosition(0.666);
-
-        telemetry.addData("Servo Position", board.getServoPosition());
+        telemetry.addData("Servo Position", light.getServoPosition());
         telemetry.addLine("Press the following buttons");
         telemetry.addLine("A: Red");
         telemetry.addLine("B: Yellow");
         telemetry.addLine("X: Green");
         telemetry.addLine("Y: Blue");
         telemetry.addLine("");
-        telemetry.addLine("Left Stick Button: Off");
-        telemetry.addLine("");
         telemetry.addLine("D Pad:");
         telemetry.addLine("Up:    Orange");
         telemetry.addLine("Left:  Sage");
         telemetry.addLine("Right: Azure");
         telemetry.addLine("Down:  Indigo");
-        }
+        telemetry.addLine("");
+        telemetry.addLine("Left Stick Button: Off");
+    }
 }
