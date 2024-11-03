23f36f800214fcef84b982e21d9746055fb332f8
RBurns625 <rburns625@icloud.com>
2024-11-03 08:19:43 -0700

    added pot adjustment instead of gamepad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/rgbLightOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/rgbLightOpMode.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBoard;
 public class rgbLightOpMode extends OpMode {
     ProgrammingBoard board = new ProgrammingBoard();
     private static final double SERVO_DEGREES = 270.0;
-    private double rgbDegrees = 0;
+    private double rgbDegrees = 0.0;
 
     @Override
     public void init() {
@@ -22,7 +22,6 @@ public class rgbLightOpMode extends OpMode {
         telemetry.addLine("X: Green");
         telemetry.addLine("Y: Blue");
         telemetry.addLine("Use 'Right Trigger' to go through spectrum");
-
         setServoToAngle(rgbDegrees);
     }
 
@@ -31,32 +30,35 @@ public class rgbLightOpMode extends OpMode {
         if(gamepad1.a) {
             board.setServoPosition(0.277);
             telemetry.addLine("Color: Red");
-            telemetry.update();
         }
         else if(gamepad1.b) {
             board.setServoPosition(0.388);
             telemetry.addLine("Color: Yellow");
-            telemetry.update();
         }
-        else if(gamepad1.x) {
+        if(gamepad1.x) {
             board.setServoPosition(0.500);
             telemetry.addLine("Color: Green");
-            telemetry.update();
         }
-        else if(gamepad1.y) {
+        if(gamepad1.y) {
             board.setServoPosition(0.611);
             telemetry.addLine("Color: Blue");
-            telemetry.update();
         }
-        else if (gamepad1.dpad_down) {
+        if (gamepad1.dpad_down) {
             board.setServoPosition(0.0);
             telemetry.addLine("Color: None");
-            telemetry.update();}
+        }
 
-            board.setServoPosition(gamepad1.right_trigger);
+        board.setServoPosition(board.getPotRange());
+        telemetry.addData("Pot Position", board.getPotRange());
 
-            telemetry.addData("Servo Position", board.getServoPosition());
-            telemetry.update();
+        telemetry.addData("Servo Position", board.getServoPosition());
+        telemetry.addLine("Press the following buttons");
+        telemetry.addLine("A: Red");
+        telemetry.addLine("B: Yellow");
+        telemetry.addLine("X: Green");
+        telemetry.addLine("Y: Blue");
+        telemetry.addLine("Use 'Right Trigger' to go through spectrum");
+        telemetry.update();
         }
     private void setServoToAngle(double degrees) {
         degrees += SERVO_DEGREES / 2;
