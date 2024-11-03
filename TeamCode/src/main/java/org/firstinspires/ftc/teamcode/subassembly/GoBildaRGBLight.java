d58430097484d3051862d576fdef45b24e75c33f
RBurns625 <rburns625@icloud.com>
2024-11-03 15:06:24 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassembly/GoBildaRGBLight.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.subassembly;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@SuppressWarnings("unused")
+public class GoBildaRGBLight {
+    private Servo rgbLight;
+
+    public void init(HardwareMap hwMap) {
+        rgbLight = hwMap.get(Servo.class, "rgbLight");
+        setServoToAngle(0);
+    }
+
+    public void setColorBlack()  { setServoToAngle(0.000); }
+    public void setColorRed()    { setServoToAngle(0.277); }
+    public void setColorYellow() { setServoToAngle(0.388); }
+    public void setColorGreen()  { setServoToAngle(0.500); }
+    public void setColorBlue()   { setServoToAngle(0.611); }
+    public void setColorOrange() { setServoToAngle(0.333); }
+    public void setColorSage()   { setServoToAngle(0.444); }
+    public void setColorAzure()  { setServoToAngle(0.555); }
+    public void setColorIndigo() { setServoToAngle(0.666); }
+
+    public double getServoPosition() { return rgbLight.getPosition(); }
+
+    public void setServoToAngle(double degrees) {
+        rgbLight.setPosition(degrees);
+    }
+}
