0d9c4cf808caff6b0317891e37fc885ceff6ee8d
RBurns625 <rburns625@icloud.com>
2024-11-03 13:07:29 -0700

    modified If statements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RgbLightOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RgbLightOpMode.java
@@ -19,38 +19,16 @@ public class RgbLightOpMode extends OpMode {
 
     @Override
     public void loop() {
-        /*
-        * Is there a cleaner way to write a list of 'if' statements?
-         */
 
-        if(gamepad1.a) {
-            // telemetry servo position is slightly off (0.2767) so it might not turn on the LED?
-            board.setServoPosition(0.277);
-        }
-        if(gamepad1.b) {
-            board.setServoPosition(0.388);
-        }
-        if(gamepad1.x) {
-            board.setServoPosition(0.500);
-        }
-        if(gamepad1.y) {
-            board.setServoPosition(0.611);
-        }
-        if (gamepad1.left_stick_button) {
-            board.setServoPosition(0.0);
-        }
-        if (gamepad1.dpad_up) {
-            board.setServoPosition(0.333);
-        }
-        if (gamepad1.dpad_left) {
-            board.setServoPosition(0.444);
-        }
-        if (gamepad1.dpad_right) {
-            board.setServoPosition(0.555);
-        }
-        if (gamepad1.dpad_down) {
-            board.setServoPosition(0.666);
-        }
+        if(gamepad1.a) board.setServoPosition(0.277);
+        if (gamepad1.b) board.setServoPosition(0.388);
+        if (gamepad1.x) board.setServoPosition(0.500);
+        if (gamepad1.y) board.setServoPosition(0.611);
+        if (gamepad1.left_stick_button) board.setServoPosition(0.0);
+        if (gamepad1.dpad_up) board.setServoPosition(0.333);
+        if (gamepad1.dpad_left) board.setServoPosition(0.444);
+        if (gamepad1.dpad_right) board.setServoPosition(0.555);
+        if (gamepad1.dpad_down) board.setServoPosition(0.666);
 
         telemetry.addData("Servo Position", board.getServoPosition());
         telemetry.addLine("Press the following buttons");
