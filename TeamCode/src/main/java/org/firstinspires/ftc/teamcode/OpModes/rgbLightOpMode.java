9ef726f0fbd2f882f8c706b0eae7e4b3848a1181
RBurns625 <rburns625@icloud.com>
2024-11-03 09:05:46 -0700

    added other colors to D-Pad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/rgbLightOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/rgbLightOpMode.java
@@ -10,46 +10,47 @@ import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBoard;
 
 public class rgbLightOpMode extends OpMode {
     ProgrammingBoard board = new ProgrammingBoard();
-    private static final double SERVO_DEGREES = 270.0;
-    private double rgbDegrees = 0.0;
 
     @Override
     public void init() {
         board.init(hardwareMap);
-        telemetry.addLine("Press the following buttons");
-        telemetry.addLine("A: Red");
-        telemetry.addLine("B: Yellow");
-        telemetry.addLine("X: Green");
-        telemetry.addLine("Y: Blue");
-        telemetry.addLine("Use 'Right Trigger' to go through spectrum");
-        setServoToAngle(rgbDegrees);
+        board.setServoPosition(0.0);
     }
 
     @Override
     public void loop() {
+        /*
+        * Is there a cleaner way to write a list of 'if' statements?
+         */
+
         if(gamepad1.a) {
+            // telemetry servo position is slightly off (0.2767) so it might not turn on the LED?
             board.setServoPosition(0.277);
-            telemetry.addLine("Color: Red");
         }
         if(gamepad1.b) {
             board.setServoPosition(0.388);
-            telemetry.addLine("Color: Yellow");
         }
         if(gamepad1.x) {
             board.setServoPosition(0.500);
-            telemetry.addLine("Color: Green");
         }
         if(gamepad1.y) {
             board.setServoPosition(0.611);
-            telemetry.addLine("Color: Blue");
+        }
+        if (gamepad1.left_stick_button) {
+            board.setServoPosition(0.0);
+        }
+        if (gamepad1.dpad_up) {
+            board.setServoPosition(0.333);
+        }
+        if (gamepad1.dpad_left) {
+            board.setServoPosition(0.444);
+        }
+        if (gamepad1.dpad_right) {
+            board.setServoPosition(0.555);
         }
         if (gamepad1.dpad_down) {
-            board.setServoPosition(0.0);
-            telemetry.addLine("Color: None");
+            board.setServoPosition(0.666);
         }
-
-        board.setServoPosition(board.getPotRange());
-        telemetry.addData("Pot Position", board.getPotRange());
 
         telemetry.addData("Servo Position", board.getServoPosition());
         telemetry.addLine("Press the following buttons");
@@ -57,11 +58,13 @@ public class rgbLightOpMode extends OpMode {
         telemetry.addLine("B: Yellow");
         telemetry.addLine("X: Green");
         telemetry.addLine("Y: Blue");
-        telemetry.addLine("Use 'Right Trigger' to go through spectrum");
-        telemetry.update();
+        telemetry.addLine("");
+        telemetry.addLine("Left Stick Button: Off");
+        telemetry.addLine("");
+        telemetry.addLine("D Pad:");
+        telemetry.addLine("Up:    Orange");
+        telemetry.addLine("Left:  Sage");
+        telemetry.addLine("Right: Azure");
+        telemetry.addLine("Down:  Indigo");
         }
-    private void setServoToAngle(double degrees) {
-        degrees += SERVO_DEGREES / 2;
-        board.setServoPosition(degrees / SERVO_DEGREES);
-    }
 }
