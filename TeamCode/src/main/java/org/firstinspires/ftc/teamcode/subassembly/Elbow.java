b69c7ac21be55d09a81a2c07d47733458d3f8bf4
RBurns625 <rburns625@icloud.com>
2024-11-05 15:32:29 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassembly/Elbow.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.subassembly;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+@SuppressWarnings("unused")
+
+public class Elbow {
+    private Servo servo;
+    private double currentAngle;
+    private static final double SERVO_DEGREES  = 180;
+    private static final double MIN_SAFE_ANGLE = -90;
+    private static final double MAX_SAFE_ANGLE = 0;
+
+    public Elbow(HardwareMap hwMap) {
+        servo = hwMap.get(Servo.class, "elbow");
+    }
+
+    public void setServoToAngle(double degrees) {
+        degrees = Range.clip(degrees, MIN_SAFE_ANGLE, MAX_SAFE_ANGLE);
+        currentAngle = degrees;
+        servo.setPosition(Range.scale(degrees, -SERVO_DEGREES / 2, SERVO_DEGREES /2, 0, 1));
+    }
+
+    public void elbowStraight() {setServoToAngle(-90);}
+    public void elbowDown()     {setServoToAngle(0);}
+    public void elbow45()       {setServoToAngle(-45);}
+
+    public double getCurrentAngle(){return currentAngle;}
+
+    public void adjustAngle(double degrees) {
+        setServoToAngle(currentAngle + degrees);
+    }
+    }
