df33a09c60e5c19fbdcf12e5683f1920b3959c7f
RBurns625 <rburns625@icloud.com>
2024-11-10 12:59:02 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ElbowTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subassembly.Elbow;
+
+@SuppressWarnings("unused")
+@TeleOp
+public class ElbowTest extends OpMode {
+    private Elbow elbow;
+
+    @Override
+    public void init() {
+        elbow = new Elbow(hardwareMap);
+        elbow.zero();
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.a) {
+            elbow.zero();
+        }
+        if(gamepad1.y) {
+            elbow.straight();
+        }
+        if (gamepad1.b) {
+            elbow.down();
+        }
+        if (gamepad1.x) {
+            elbow.angled45();
+        }
+        elbow.adjustAngle(-gamepad1.left_trigger  * 0.75);
+        elbow.adjustAngle( gamepad1.right_trigger * 0.75);
+
+        elbow.outputTelemetry(telemetry);
+    }
+
+}
