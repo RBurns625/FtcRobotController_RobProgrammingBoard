4bf69626f3d839e978cadf78a3e8d9f4d766805a
RBurns625 <rburns625@icloud.com>
2024-11-10 11:45:36 -0700

    added new servo class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassembly/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassembly/Wrist.java
@@ -2,26 +2,22 @@ package org.firstinspires.ftc.teamcode.subassembly;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @SuppressWarnings("unused")
 
-public class Wrist {
-    private Servo servo;
-    private boolean isStraight;
-    private double currentAngle;
-    private static final double SERVO_DEGREES    =  180;
+public class Wrist extends ServoSubassembly{
     private static final double MIN_SAFE_DEGREES =  -90;
     private static final double MAX_SAFE_DEGREES =   90;
+    private boolean isStraight;
 
     public Wrist(HardwareMap hwMap) {
-        servo = hwMap.get(Servo.class, "wrist");
+        super(MIN_SAFE_DEGREES, MAX_SAFE_DEGREES, hwMap.get(Servo.class, "Wrist"));
     }
 
     public void isStraight() {
-        setServoToAngle(-90);
+        setServoToAngle(0);
         isStraight = true;
     }
     public void zero() {
@@ -33,23 +29,26 @@ public class Wrist {
         isStraight = true;
     }
     public void right() {
-        setServoToAngle(90);
-        isStraight = false;
-    }
-    public void left() {
         setServoToAngle(-90);
         isStraight = false;
     }
-
-    public void setServoToAngle(double degrees) {
-        degrees = Range.clip(degrees, MIN_SAFE_DEGREES, MAX_SAFE_DEGREES);
-        currentAngle = degrees;
-        servo.setPosition(Range.scale(degrees, -SERVO_DEGREES / 2, SERVO_DEGREES / 2, 0, 1));
+    public void left() {
+        setServoToAngle(90);
+        isStraight = false;
+    }
+    public void toggleAngle() {
+        if (isStraight) {
+            right();
+        }
+        else {
+            straight();
+        }
     }
 
-    public double getCurrentAngle() {return currentAngle;}
-
-    public void adjustAngle(double degrees) { setServoToAngle(currentAngle+degrees); }
+    public void adjustAngle(double degrees) {
+        setServoToAngle(currentAngle+degrees);
+        isStraight = (currentAngle > -1 && currentAngle < 1);
+    }
 
     public void outputTelemetry(Telemetry telemetry) {
         telemetry.addData("Wrist Angle", currentAngle);
