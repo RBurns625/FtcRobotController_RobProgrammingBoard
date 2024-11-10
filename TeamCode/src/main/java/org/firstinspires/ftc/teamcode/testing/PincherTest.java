20f84c17f0ede95e3ff07770181e170760014179
RBurns625 <rburns625@icloud.com>
2024-11-10 12:28:23 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/PincherTest.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subassembly.Pincher;
+
+@SuppressWarnings("unused")
+
+@TeleOp
+public class PincherTest extends OpMode {
+    private Pincher pincher;
+
+    @Override
+    public void init() {
+        pincher = new Pincher(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.x) {
+            pincher.zero();
+        }
+        if(gamepad1.a) {
+            pincher.open();
+        }
+        if (gamepad1.b) {
+            pincher.closed();
+        }
+
+        pincher.adjustAngle(-gamepad1.left_trigger * 0.25);
+
+        pincher.adjustAngle(gamepad1.left_trigger * 0.25);
+
+        pincher.outputTelemetry(telemetry);
+        telemetry.update();
+    }
+}
